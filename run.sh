c57af454cd6cbd3a3eaf448d19cb94fa4631d987
map-A <test@zhifubaobao.ml>
2023-11-01 10:27:53 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,29 +1,29 @@
 #!/bin/sh
 # 定义文件路径变量
-base_path="D:\\work\\3DMark_FireStrike-Extreme-orgSize-FixFPS0.5-GT2\\"
-first_secene=146
-save_secene=146
+base_path="D:\\work\\3DMark_FireStrike-orgSize-FixFPS0.5-GT1\\"
+first_secene=112
+save_secene=119
 number=1
-path1="${base_path}\\vector\\"
-path2="${base_path}\\vector\\3DMarkICFWorkload_0.sdx"
 
 
-echo "dumping frame"
-./qReplay/qReplay.exe -s $path2 --dumpF --hide
-mv $base_path\\ReplayDump\\ $base_path\\Frame_dump_origin\\
+# echo "dumping frame"
+# ./qReplay/qReplay.exe -s $path2 --dumpF --hide
+# mv $base_path\\ReplayDump\\ $base_path\\Frame_dump_origin\\
 
-simple_nums=21
+simple_nums=1
 for (( i=1; i<=$simple_nums; i++ )); do
     echo $save_secene
     # 使用变量执行命令
-    echo | python ./FrameSimplifier.py -s $path1 -f $first_secene -b $save_secene -n $number -m -1
+    echo | python ./FrameSimplifier.py -s $base_path -f $first_secene -b $save_secene -n $number
+
     mv $base_path\\frames\\ $base_path\\vector-F$save_secene\\
+    
     mv $base_path\\ReplayDump\\ $base_path\\ReplayDump-F$save_secene\\
+    
     sed -i '2s#axPath(Path = "..\\data\\; ..\\shader\\; ..\\LogDump\\");#axPath(Path = "..\\data\\; ..\\shader\\; ..\\LogDump\\;..\\ReplayDump-F'"$save_secene"'\\HW\\");#' $base_path\\vector-F$save_secene\\3DMarkICFWorkload_F${save_secene}_0.sdx
 
-
-    find $path1 -name "*.inject" -type f -delete
-    rm -rf $base_path\\temp\\
+    find $base_path\\vector\\ -name "*.inject" -type f -delete
+    # rm -rf $base_path\\temp\\
     save_secene=$((save_secene+1))
 done
 
